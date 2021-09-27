cc27941fc8a73d1111b819c32df3ec592804ad01
alesandramempis <alesandramempis@gmail.com>
2021-09-27 11:30:26 -0300

    Implementação do event ao digitar letras

--- src/app/conversor/directives/numero.directive.ts
+++ src/app/conversor/directives/numero.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[numero]'
@@ -6,5 +6,24 @@ import { Directive } from '@angular/core';
 export class NumeroDirective {
 
   constructor() { }
+
+  /**
+   * Implementa evento de keyup para o elemento da diretiva.
+   * 
+   * @param any $event
+   */
+  @HostListener('keyup', ['$event'])onKeyUp($event: any) {
+    let valor = $event.target.value;
+    let posDecimais = valor.indexOf('.');
+
+    valor = valor.replace(/[\D]/g, '');
+
+    if(posDecimais > 0) {
+      valor = valor.substr(0, posDecimais) + '.' + 
+      valor.substr(posDecimais);
+    }
+
+    $event.target.value = valor;
+  }
 
 }
