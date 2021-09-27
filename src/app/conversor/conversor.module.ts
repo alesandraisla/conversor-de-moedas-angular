06e7e7568c29450bd9210cca88be622ba4db88db
alesandramempis <alesandramempis@gmail.com>
2021-09-27 09:43:50 -0300

    Criação do serviço de conversão de moeda

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,12 +1,24 @@
+import { MoedaService, ConversorService } from './services';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ConversorComponent } from './components';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ConversorComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [
+    ConversorComponent
+  ],
+  providers: [
+    MoedaService,
+    ConversorService
   ]
 })
 export class ConversorModule { }
